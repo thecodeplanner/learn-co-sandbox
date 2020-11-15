8d16b210f8be95d79f582faa17e8384f16e58dac
Melissa <thecodeplanner@gmail.com>
2020-11-15 22:05:07 +0000

    Automatically backed up by Learn

---
+++ vending_machine.rb
@@ -0,0 +1,12 @@
+# test vending machine of mixed nested data structures
+
+vending_machine = [
+  [chocolate, {
+    :name => "Hershey",
+    :price => 2},
+    {:name => "KitKat",
+    :price => 1.5}]
+  nuts 
+  chips, 
+  candy 
+  ]
